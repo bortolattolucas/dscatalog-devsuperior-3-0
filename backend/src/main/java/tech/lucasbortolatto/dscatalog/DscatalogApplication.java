dd74bda7be9eddca72083229912fa1d62360516e
Lucas Bortolatto <lucas-bortolatto@outlook.com>
2021-10-19 21:02:57 -0300

    Backend project created

---
+++ backend/src/main/java/tech/lucasbortolatto/dscatalog/DscatalogApplication.java
@@ -0,0 +1,13 @@
+package tech.lucasbortolatto.dscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/tech/lucasbortolatto/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.lucasbortolatto.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
