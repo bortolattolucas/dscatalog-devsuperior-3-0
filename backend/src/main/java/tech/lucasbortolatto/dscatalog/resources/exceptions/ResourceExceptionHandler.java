6b33e00e68740eac99139418a592012ead85f24e
Lucas Bortolatto <lucas-bortolatto@outlook.com>
2021-10-20 19:46:20 -0300

    Update Category

--- backend/src/main/java/tech/lucasbortolatto/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/tech/lucasbortolatto/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import tech.lucasbortolatto.dscatalog.services.exceptions.EntityNotFoundException;
+import tech.lucasbortolatto.dscatalog.services.exceptions.ResourceNotFoundException;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.Instant;
@@ -13,8 +13,8 @@ import java.time.Instant;
 public class ResourceExceptionHandler {
 
     //Toda exception desse tipo estourada nos controllers vai cair no método abaixo
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request) {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         StandardError err = new StandardError();
         err.setTimestamp(Instant.now());
         err.setStatus(HttpStatus.NOT_FOUND.value());
--- backend/src/main/java/tech/lucasbortolatto/dscatalog/services/CategoryService.java
+++ backend/src/main/java/tech/lucasbortolatto/dscatalog/services/CategoryService.java
@@ -6,8 +6,9 @@ import org.springframework.transaction.annotation.Transactional;
 import tech.lucasbortolatto.dscatalog.dto.CategoryDTO;
 import tech.lucasbortolatto.dscatalog.entities.Category;
 import tech.lucasbortolatto.dscatalog.repositories.CategoryRepository;
-import tech.lucasbortolatto.dscatalog.services.exceptions.EntityNotFoundException;
+import tech.lucasbortolatto.dscatalog.services.exceptions.ResourceNotFoundException;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -26,7 +27,7 @@ public class CategoryService {
     @Transactional(readOnly = true)
     public CategoryDTO findById(Long id) {
         Optional<Category> obj = categoryRepository.findById(id);
-        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+        Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
         return new CategoryDTO(entity);
     }
 
@@ -36,5 +37,19 @@ public class CategoryService {
         entity.setName(dto.getName());
         entity = categoryRepository.save(entity);
         return new CategoryDTO(entity);
+    }
+
+    @Transactional
+    public CategoryDTO update(Long id, CategoryDTO dto) {
+        try {
+            //getOne não cria uma consulta adicional ao banco, ao invés disso apenas cria um objeto provisório p/ transação
+            Category entity = categoryRepository.getOne(id);
+            entity.setName(dto.getName());
+            entity = categoryRepository.save(entity);
+            return new CategoryDTO(entity);
+        } catch (EntityNotFoundException e) {
+            //caso o getOne dentro da transação não encontrar o recurso ele dispara essa EntityNotFoundException
+            throw new ResourceNotFoundException("Entity not found with id " + id);
+        }
     }
 }
