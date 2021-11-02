b7c3a72b29c1ebc671e637046cdc70ec4dd988aa
Lucas Bortolatto <lucas-bortolatto@outlook.com>
2021-11-02 00:11:42 -0300

    H2 Security Configuration

--- backend/src/main/java/tech/lucasbortolatto/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/tech/lucasbortolatto/dscatalog/config/ResourceServerConfig.java
@@ -2,6 +2,7 @@ package tech.lucasbortolatto.dscatalog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -9,13 +10,20 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+import java.util.Arrays;
+
 // configuração do ResourceServer do checklist do oauth
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
+    // Objeto que representa o ambiente de execução da aplicação
+    // Instanciado para liberar acesso ao H2 console
+    @Autowired
+    private Environment environment;
+
     // rotas abertas para todos
-    private static final String[] PUBLIC = {"/oauth/token"};
+    private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"};
 
     // rotas abertas para operadores e administradores
     private static final String[] OPERATOR_OR_ADMIN = {"/products/**", "/categories/**"};
@@ -34,6 +42,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+        // libera o acesso ao h2 console
+        if (Arrays.asList(environment.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
+
         http.authorizeRequests() // configura as autorizações por rotas/roles
                 .antMatchers(PUBLIC).permitAll() // public liberado para todos, com ou sem roles
                 .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()  // permite apenas o método GET para todos nessa rota, com ou sem roles
