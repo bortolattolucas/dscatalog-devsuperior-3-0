c84d4a6ba6a8fd90411b0942c3cd1bb9215ada84
Lucas Bortolatto <lucas-bortolatto@outlook.com>
2021-11-03 22:02:24 -0300

    Configurating CORS

--- backend/src/main/java/tech/lucasbortolatto/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/tech/lucasbortolatto/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package tech.lucasbortolatto.dscatalog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +12,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
 
@@ -53,5 +60,36 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                 .antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN") // permite todos os métodos nessas rotas pra quem tiver essas roles, sem precisar do prefixo do banco "ROLE_"
                 .antMatchers(ADMIN).hasRole("ADMIN") // permite todos os métodos nessas rotas para quem tiver essa role
                 .anyRequest().authenticated(); // qualquer outra rota, precisa estar autenticado
+
+        // configura o cors com o bean criado abaixo
+        http.cors().configurationSource(corsConfigurationSource());
     }
+
+    /*
+        Componentes para configuração de CORS no ResourceServer
+        MUITA ATENÇÃO AOS IMPORTS
+     */
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        // na linha abaixo ta liberando geral de qualquer lugar fazer request
+        corsConfig.setAllowedOriginPatterns(Arrays.asList("*"));
+//        corsConfig.setAllowedOriginPatterns(Arrays.asList("https://meudominio.com"));
+        corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
+        corsConfig.setAllowCredentials(true);
+        corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return source;
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        FilterRegistrationBean<CorsFilter> bean
+                = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+
 }
