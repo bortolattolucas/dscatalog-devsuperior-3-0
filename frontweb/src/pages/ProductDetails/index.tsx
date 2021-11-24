0ff7c1be3096696864c172f15e637dcd814b5148
Lucas Bortolatto <lucas-bortolatto@outlook.com>
2021-11-23 21:37:21 -0300

    UseState, UseEffect, UseParams

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,9 +1,30 @@
 import {ReactComponent as ArrowIcon} from 'assets/images/arrow.svg';
-import {Link} from 'react-router-dom';
+import {Link, useParams} from 'react-router-dom';
 import ProductPrice from "../../components/ProductPrice";
 import './styles.css';
+import {Product} from "../../types/product";
+import axios from "axios";
+import {useEffect, useState} from "react";
+import {BASE_URL} from "../../util/requests";
+
+
+type UrlParams = {
+    productId: string;
+}
 
 const ProductDetails = () => {
+
+    const {productId} = useParams<UrlParams>();
+
+    const [product, setProduct] = useState<Product>();
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/products/${productId}`)
+            .then(response => {
+                setProduct(response.data);
+            });
+    }, [productId]); //Observer, qdo um parametro das dependencias muda, o componente carrega de novo
+
     return (
         <div className="product-details-container">
             <div className="base-card product-details-card">
@@ -17,19 +38,18 @@ const ProductDetails = () => {
                     <div className="col-xl-6">
                         <div className="img-container">
                             <img
-                                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg"
-                                alt="Produto"/>
+                                src={product?.imgUrl}
+                                alt={product?.name}/>
                         </div>
                         <div className="name-price-container">
-                            <h1>Nome do Produto</h1>
-                            <ProductPrice price={2345.64}/>
+                            <h1>{product?.name}</h1>
+                            {product && <ProductPrice price={product?.price}/>}
                         </div>
                     </div>
                     <div className="col-xl-6">
                         <div className="description-container">
                             <h2>Descrição do produto</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate deserunt dolore
-                                doloribus eius impedit ipsum natus non nulla, officia repudiandae!</p>
+                            <p>{product?.description}</p>
                         </div>
                     </div>
                 </div>
